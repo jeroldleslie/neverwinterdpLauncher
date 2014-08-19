bca73f07a10084a04e8d36e2109e7ecf55e5b46d
Peter Jerold Leslie <leslie@charlietech.com>
2014-08-19 12:17:16 +0530

    files changed

--- setup_digitalOcean.sh
+++ setup_digitalOcean.sh
@@ -1,11 +1,46 @@
 set -x
-git clone https://github.com/DemandCube/DemandCubePlaybooks.git 
+git clone https://github.com/jeroldleslie/DemandCubePlaybooks.git
 cd DemandCubePlaybooks
 git clone https://github.com/DemandCube/ansible-flow.git
 mv -f  ./ansible-flow/library/ ./library
 rm -rf ./ansible-flow
 
-cd ../
+rm -rf ./roles/neverwinterdp/files
+
+
+mkdir ./roles/neverwinterdp/files
+mkdir ./roles/neverwinterdp/files/workspace
+
+cd ./roles/neverwinterdp/files/workspace
+
+git clone https://github.com/DemandCube/NeverwinterDP-Commons
+git clone https://github.com/DemandCube/Queuengin
+git clone https://github.com/DemandCube/Sparkngin
+git clone https://github.com/DemandCube/Scribengin
+git clone https://github.com/DemandCube/Demandspike
+git clone https://github.com/DemandCube/NeverwinterDP
+
+cd NeverwinterDP-Commons
+gradle clean build install
+
+cd ../Queuengin
+gradle clean build install
+
+cd ../Sparkngin
+gradle clean build install
+
+cd ../Scribengin
+gradle clean build install
+
+cd ../Demandspike
+gradle clean build install
+
+cd ../NeverwinterDP
+gradle clean build install release
+
+cd ../../../../../../
+
+
 rm -rf Vagrantfile
 vagrant plugin install vagrant-flow
 vagrant flow multiinit
